d959b489ba3aa0a5c164b5f6fd50e7ffe9b0239e
himansumuduli <97511041+himansumuduli@users.noreply.github.com>
2024-07-20 19:39:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let angle = 0;
+
+function rotateLine() {
+    const line = document.getElementById('line');
+    angle = (angle + 2) % 360; 
+    line.style.transform = `rotate(${angle}deg)`;
+    requestAnimationFrame(rotateLine);
+}
+
+rotateLine();
+
